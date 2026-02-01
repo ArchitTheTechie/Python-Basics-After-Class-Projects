3f1b14cd1daa99340dcefc883bbb74c4eb2c3ae9
Archit Anant <maneesha.roy09@gmail.com>
2026-02-01 11:25:37 +0530

    Add files via upload

---
+++ sorting birthday.py
@@ -0,0 +1,22 @@
+# Sorting birthday
+
+Archit = "28/01/2014"
+Shaurya = "24/09/2013"
+Maneesha = "27/09/1984"
+Ravi = "10/11/1980"
+Siddhant = "14/12/2013"
+Hadeed = "28/07/2013"
+Vihaan_Puri = "22/09/2013"
+Joyce = "12/02/1983"
+Aadya = "14/02/2013"
+Arjun = "12/01/2014"
+print("Archit's birthday falls on :", Archit)
+print("Shaurya's birthday falls on :", Shaurya)
+print("Maneesha's birthday falls on :", Maneesha)
+print("Ravi's birthday falls on :", Ravi)
+print("Siddhant's birthday falls on ;", Siddhant)
+print("Hadeed'birthday falls on ;", Hadeed)
+print("Vihaan Puri's birthday falls on :", Vihaan_Puri)
+print("Joyce's birthday falls on :", Joyce)
+print("Aadya's birthday falls on :", Aadya)
+print("Arjun's birthday falls on :", Arjun)
