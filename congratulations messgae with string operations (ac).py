c251b8a1ab441c45ac34796a73b7f2b6e8ee055e
Archit Anant <maneesha.roy09@gmail.com>
2026-02-03 19:24:27 +0530

    Add files via upload

---
+++ congratulations messgae with string operations (ac).py
@@ -0,0 +1,7 @@
+name = "Archit"
+achievement = "getting good marks in the exam"
+
+# Concatenate strings and variables
+message = "Congratulations, " + name + "! You did a great job " + achievement + "."
+
+print(message)
