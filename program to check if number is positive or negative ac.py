4f7aba68f327462f3a2bf1758be6bd427542660a
Archit Anant <maneesha.roy09@gmail.com>
2026-02-08 12:07:19 +0530

    Add files via upload

---
+++ program to check if number is positive or negative ac.py
@@ -0,0 +1,10 @@
+#Program to check whether the number is positive or negative or neutral
+
+number = int(input("Enter a number : "))
+
+if number > 0:
+    print("The number is Positive")
+elif number < 0:
+    print("The number is Negative")
+else:
+    print("The number is zero")
